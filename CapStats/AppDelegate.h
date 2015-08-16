f24d1a06958d887f9e7f12c466ec363b85681b0e
aco711 <alexcohen711@gmail.com>
2015-08-16 18:03:50 -0400

    Initial Commit

---
+++ CapStats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CapStats
+//
+//  Created by Alex Cohen on 8/16/15.
+//  Copyright (c) 2015 Alex Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
